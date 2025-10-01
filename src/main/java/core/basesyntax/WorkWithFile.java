bc855103ff5d26ea24608046d0786b8791649653
mr-Edoc <mr.edoc.work@gmail.com>
2025-10-01 12:14:40 +0300

    Ver1/Developed getStatistic method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,47 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
+    static final String SUPPLY_NAME = "supply";
+
     public void getStatistic(String fromFileName, String toFileName) {
+        int supplyAmount = 0;
+        int buyAmount = 0;
+        String[] linePart;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+            String value = reader.readLine();
+
+            while (value != null) {
+                linePart = value.split(",");
+                if (linePart[0].equals(SUPPLY_NAME)) {
+                    supplyAmount += Integer.parseInt(linePart[1]);
+                } else {
+                    buyAmount += Integer.parseInt(linePart[1]);
+                }
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file", e);
+        }
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
+            writer.write("supply," + supplyAmount);
+            writer.newLine();
+            writer.write("buy," + buyAmount);
+            writer.newLine();
+            writer.write("result," + (supplyAmount - buyAmount));
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write into file",e);
+        }
 
     }
 }
