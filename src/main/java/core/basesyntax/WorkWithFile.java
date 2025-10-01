db26fbc88a338df2de568453258c8a5268cbde34
mr-Edoc <mr.edoc.work@gmail.com>
2025-10-01 13:55:58 +0300

    Ver1.2/Improved checkstyle mistakes, added constants, added more tests parts to prevent mistakes in file ruin code flow

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,14 +9,16 @@ import java.io.IOException;
 public class WorkWithFile {
     static final String SUPPLY_NAME = "supply";
     static final String BUY_NAME = "buy";
+    static final String RESULT_NAME = "result";
     static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         int supplyAmount = 0;
         int buyAmount = 0;
         String operation;
-        int amountStr;
+        int amount;
         String[] linePart;
+        StringBuilder report = new StringBuilder();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String value = reader.readLine();
@@ -25,11 +27,11 @@ public class WorkWithFile {
                 linePart = value.split(COMMA);
                 if (linePart.length == 2) {
                     operation = linePart[0].trim();
-                    amountStr = Integer.parseInt(linePart[1].trim());
+                    amount = Integer.parseInt(linePart[1].trim());
                     if (operation.equals(SUPPLY_NAME)) {
-                        supplyAmount += amountStr;
+                        supplyAmount += amount;
                     } else if (operation.equals(BUY_NAME)) {
-                        buyAmount += amountStr;
+                        buyAmount += amount;
                     } else {
                         throw new RuntimeException("Operation does not exist");
                     }
@@ -40,18 +42,16 @@ public class WorkWithFile {
                 value = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read file", e);
+            throw new RuntimeException("Can`t read file: " + fromFileName, e);
         }
+        report.append(SUPPLY_NAME + COMMA).append(supplyAmount).append(System.lineSeparator())
+                .append(BUY_NAME + COMMA).append(buyAmount).append(System.lineSeparator())
+                .append(RESULT_NAME + COMMA).append(supplyAmount - buyAmount);
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-
-            writer.write("supply," + supplyAmount);
-            writer.newLine();
-            writer.write("buy," + buyAmount);
-            writer.newLine();
-            writer.write("result," + (supplyAmount - buyAmount));
+            writer.append(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can`t write into file",e);
+            throw new RuntimeException("Can`t write into file: " + toFileName,e);
         }
 
     }
