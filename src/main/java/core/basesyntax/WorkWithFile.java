1b85a5beb53f9bd60c4a6e1b13f387bdc57f6224
mr-Edoc <mr.edoc.work@gmail.com>
2025-10-01 13:01:16 +0300

    Ver1.1/Improved checkstyle mistakes, added constants, added more tests parts to prevent mistakes in file ruin code flow

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,37 +8,48 @@ import java.io.IOException;
 
 public class WorkWithFile {
     static final String SUPPLY_NAME = "supply";
+    static final String BUY_NAME = "buy";
+    static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         int supplyAmount = 0;
         int buyAmount = 0;
+        String operation;
+        int amountStr;
         String[] linePart;
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String value = reader.readLine();
 
             while (value != null) {
-                linePart = value.split(",");
-                if (linePart[0].equals(SUPPLY_NAME)) {
-                    supplyAmount += Integer.parseInt(linePart[1]);
+                linePart = value.split(COMMA);
+                if (linePart.length == 2) {
+                    operation = linePart[0].trim();
+                    amountStr = Integer.parseInt(linePart[1].trim());
+                    if (operation.equals(SUPPLY_NAME)) {
+                        supplyAmount += amountStr;
+                    } else if (operation.equals(BUY_NAME)) {
+                        buyAmount += amountStr;
+                    } else {
+                        throw new RuntimeException("Operation does not exist");
+                    }
                 } else {
-                    buyAmount += Integer.parseInt(linePart[1]);
+                    throw new RuntimeException("Line incorrect");
                 }
+
                 value = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can`t read file", e);
         }
 
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+
             writer.write("supply," + supplyAmount);
             writer.newLine();
             writer.write("buy," + buyAmount);
             writer.newLine();
             writer.write("result," + (supplyAmount - buyAmount));
-            writer.close();
         } catch (IOException e) {
             throw new RuntimeException("Can`t write into file",e);
         }
